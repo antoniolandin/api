01705bdd2f0251aab3a1f841ea975197b83b381e
Pastafarista <antoniolandin@proton.me>
2024-04-24 14:14:14 +0200

    Feat: creado middleware

--- middleware/session.js
+++ middleware/session.js
@@ -1,1 +1,27 @@
+const { handleHttpError } = require('../utils/handleError');
 const { verifyToken } = require('../utils/handleJwt');
+
+const authMiddleware = async (req, res, next) => {
+    try {
+        if (!req.headers.authorization) {
+            return handleHttpError(res, 'No token provided', 401)
+            return
+        }
+
+        const token = req.headers.authorization.split(' ').pop();
+
+        const dataToken = await verifyToken(token);
+
+        if (!dataToken.id) {
+            return handleHttpError(res, 'Invalid token', 401)
+            return
+        }
+
+        next()
+    }
+    catch (error) {
+        return handleHttpError(res, error.message, 401)
+    }
+}
+
+module.exports = authMiddleware;
